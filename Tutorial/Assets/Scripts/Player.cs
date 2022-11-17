66dab890596bc8470ae7515219844ad130ffbd79
Pramu664 <pramudithaayeshmantha0@gmail.com>
2022-11-17 11:39:04 +0530

    add the player controller

---
+++ Tutorial/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour {
+	public float moveSpeed;
+	PlayerController controller;
+	Camera viewCamera;
+
+	void Start () {
+		controller = GetComponent<PlayerController> ();
+		viewCamera = Camera.main; 
+	}
+	
+	void Update () {
+		Vector3 input = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));	
+		Vector3 direction = input.normalized;
+		Vector3 moveVelocity = moveSpeed * direction;
+		controller.Move (moveVelocity);
+
+		Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+		Plane groundPlane = new Plane (Vector3.up, Vector3.zero);
+		float rayDistance;
+		if (groundPlane.Raycast (ray, out rayDistance)) {
+			Vector3 point = ray.GetPoint (rayDistance);
+			Debug.DrawLine (ray.origin, point, Color.red);
+			controller.LookAt (point);
+		}
+	}
+}
---
+++ Tutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+	Rigidbody myRigidbody;
+	Vector3 velocity;
+
+	void Start (){
+		myRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	public void LookAt(Vector3 lookPoint) {
+		Vector3 heightCorrectedPoint = new Vector3 (lookPoint.x, transform.position.y, lookPoint.z);
+		transform.LookAt(heightCorrectedPoint);
+		
+	}
+
+	public void Move(Vector3 _velocity){
+		velocity = _velocity;	
+	}
+
+	public void FixedUpdate() {
+		myRigidbody.MovePosition (myRigidbody.position + velocity * Time.fixedDeltaTime);
+	}
+}
